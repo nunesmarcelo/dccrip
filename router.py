4afd0a74f890594d5f387cfc3181357e20cc1b0c
Marcelo Nunes <marcelonunes3698@gmail.com>
2019-05-22 15:16:45 -0300

    del ok

--- router.py
+++ router.py
@@ -27,10 +27,13 @@ class DCCRIP:
         self.input = open(sys.argv[3], 'r') if len(sys.argv) > 3 else None
         
         # IP : Weight | IP : NextStep
-        self.routingWeightTable = {}
-        self.routingNextStepTable = {}
-        self.routingWeightTable[self.myAddress] = 0
-        self.routingNextStepTable[self.myAddress] = self.myAddress
+        # self.routingWeightTable = {}
+        # self.routingNextStepTable = {}
+        # self.routingWeightTable[self.myAddress] = 0
+        # self.routingNextStepTable[self.myAddress] = self.myAddress
+
+        self.routingTable = {}
+        self.routingTable[self.myAddress] = { 'weight' : 0 , 'next' : self.myAddress}
 
         # IP : Weight
         self.neighborsTable = {}
@@ -74,31 +77,35 @@ class DCCRIP:
             #Decode json data
             data = json.loads( bytes.decode( data) )
 
-            # If type = update -> update routingWeightTable and routingNextStepTable
+            # If type = update -> update the routingTable
             if(data['type'] == 'update'):
                 distances = data['distances']
                 
                 for address in distances:
                     #print(address, distances[address] , type(distances[address]))
 
+                    #If sender not is my neighbor , i don't can receive his package.
                     if(data['source'] not in self.neighborsTable):
                         break
 
                     # If old weight greater than new weight -> swap
-                    if ((address not in self.routingWeightTable.keys()) or int(self.routingWeightTable[address]) > int(distances[address]) + int(self.neighborsTable[data['source']])):
-                        self.routingWeightTable[address] = int(distances[address]) + int(self.neighborsTable[data['source']])
-                        self.routingNextStepTable[address] = data['source']
+                    # If ip not in routingTable, add 
+                    if ((address not in self.routingTable) or 
+                        int(self.routingTable[address]['weight']) > (int(distances[address]) + int(self.neighborsTable[data['source']])) ):
+                        self.routingTable[address] = {}
+                        self.routingTable[address]['weight'] = int(distances[address]) + int(self.neighborsTable[data['source']])
+                        self.routingTable[address]['next'] = data['source']
 
-                        logging.debug('Endereço atualizado: {} - antigo: {} - peso: {}'.format(address , int(self.routingWeightTable[address]) , int(distances[address]) + int(self.neighborsTable[data['source']])))
+                        logging.debug('Endereço atualizado: {} - antigo: {} - peso: {}'.format(address , int(self.routingTable[address]['weight']) , int(distances[address]) + int(self.neighborsTable[data['source']])))
 
-                for nextStep in self.routingNextStepTable.copy():
-                    #if the nextstep are in my tables, and uses the neighbor, but the table of neighbor don't have this, del from tables.
-                    if self.routingNextStepTable[nextStep] == data['source'] and nextStep not in distances:
-                        del self.routingWeightTable[nextStep]
-                        if nextStep in self.neighborsTable:
-                            del self.neighborsTable[nextStep]
-                        del self.routingNextStepTable[nextStep]
-                        print( nextStep, " deletado na recepção da update - linha 100")
+                # Delete the ips that are in the old routingTable, but are not on the distances received.
+                for old in self.routingTable.copy():
+                    if self.routingTable[old]['next'] == data['source'] and old not in distances:
+                        del self.routingTable[old]
+
+                # If this message is to del our connection, delete this neighbor for the neighborTable
+                if distances == {} and data['source'] in self.neighborsTable:
+                    del self.neighborsTable[data['source']]
 
             # If type = table -> send self routingWeightTable and routingNexStepTable for who requested
             if(data['type'] == 'trace' or data['type'] == 'table'):
@@ -117,7 +124,7 @@ class DCCRIP:
 
                     #The type of return is the table of this node
                     if(data['type'] == 'table'):
-                        message.update({ 'payload' : [ (ip,self.routingNextStepTable[ip],self.routingWeightTable[ip]) for ip in self.routingWeightTable ] })
+                        message.update({ 'payload' : [ (ip,self.routingTable[ip]['next'],self.routingTable[ip]['weight']) for ip in self.routingTable ] })
                     
                     #The type of return is a list of addresses
                     if(data['type'] == 'trace'):
@@ -129,30 +136,35 @@ class DCCRIP:
                     if(message['type'] == 'trace'):
                         message['hops'].append(self.myAddress)
                     
-                #print("message: " , message , ' - next: ' , self.routingNextStepTable[ message['destination'] ])
-                sock.sendto(str.encode ( json.dumps( message ) ) , ( self.routingNextStepTable[ message['destination'] ] , self.port) )
+                #print("message: " , message , ' - next: ' , self.routingTable[ message['destination'] ])
+                sock.sendto(str.encode ( json.dumps( message ) ) , ( self.routingTable[ message['destination'] ]['next'] , self.port) )
             
             if(data['type'] == 'data'):
                 if(data['destination'] == self.myAddress):
                     print(data['payload'])
                 else:
                     sock = socket.socket(socket.AF_INET , socket.SOCK_DGRAM) # UDP socket
-                    sock.sendto( str.encode(json.dumps(data)) , (self.routingNextStepTable[data['destination']] , self.port) )
+                    sock.sendto( str.encode(json.dumps(data)) , (self.routingTable[data['destination']]['next'] , self.port) )
 
                 
 
       
     def listenInputs(self):
         try:
-            readCommand = self.input.readline() if self.input != None else input() #input() if has no file open, or readline() else.
-            while readCommand:
+             while True:
+                readCommand = self.input.readline() if self.input != None else input() #input() if has no file open, or readline() else.
+
+                if(readCommand == 'quit'):
+                    os._exit(0)
+
                 # Add Neighbor
                 if readCommand.split(' ')[0] == 'add':
                     if len(readCommand.split(' ')) <= 2:
                         print("Input failure. Correct pattern:\t add <ip> <weight>")
                     else:
-                        self.routingWeightTable[readCommand.split(' ')[1] ] = readCommand.split(' ')[2]
-                        self.routingNextStepTable[readCommand.split(' ')[1] ] = readCommand.split(' ')[1]
+                        self.routingTable[readCommand.split(' ')[1] ] = {}
+                        self.routingTable[readCommand.split(' ')[1] ]['weight'] = readCommand.split(' ')[2]
+                        self.routingTable[readCommand.split(' ')[1] ]['next'] = readCommand.split(' ')[1]
                         self.neighborsTable[readCommand.split(' ')[1]] = readCommand.split(' ')[2]
                         self.sendUpdates(repeat=False)
                 
@@ -164,13 +176,21 @@ class DCCRIP:
                         if ip in self.neighborsTable: # Remove the ip from neighborhood if it is neighbor
                             del self.neighborsTable[ip]
 
-                            if ip in self.routingWeightTable: # Remove the ip from Weights if there exists 
-                                del self.routingWeightTable[ip]
-                            if ip in self.routingNextStepTable: # Remove the ip from nextsteps if there exists
-                                del self.routingNextStepTable[ip]
+                            # if ip in self.routingWeightTable: # Remove the ip from Weights if there exists 
+                            #     del self.routingWeightTable[ip]
+                            # if ip in self.routingNextStepTable: # Remove the ip from nextsteps if there exists
+                            #     del self.routingNextStepTable[ip]
+
+                            # if ip in self.routingTable.values():
+                            #     del self.routingTable[ip]
+
+                            for old in self.routingTable.copy():
+                                if ip == self.routingTable[old]['next']:
+                                    del self.routingTable[old]
+
                             self.sendUpdates(repeat=False , ipExcluded=ip) # Send the news from the others.
                         else:
-                            print('Ip not found on the tables')
+                            print('Ip not found on the neighborhood')
 
                 # Table and Trace
                 if readCommand.split(' ')[0] == 'table' or readCommand.split(' ')[0] == 'trace':
@@ -190,12 +210,11 @@ class DCCRIP:
                         if readType == 'trace':
                             message.update({ 'hops' : [self.myAddress] })
 
-                        sock.sendto(str.encode ( json.dumps( message ) ) , ( self.routingNextStepTable[destination], self.port) )
+                        sock.sendto(str.encode ( json.dumps( message ) ) , ( self.routingTable[destination]['next'], self.port) )
 
                 if readCommand.split(' ')[0] == 'print':
                     self.imprimirTabelas()
 
-                readCommand = self.input.readline() if self.input != None else input()
         except KeyboardInterrupt:
             raise KeyboardInterrupt
 
@@ -205,21 +224,26 @@ class DCCRIP:
             for key in self.neighborsTable:
 
                 # Make a dict of type -> 'ip' : 'cost'  
-                distances = self.routingWeightTable.copy()
+                distances = self.routingTable.copy()
 
                 #Split Horizon
-                if key in distances: # 1º don't send for x information to go to x
-                    del distances[key]
-                for nextStep in self.routingNextStepTable: # 2º don't send for x information that uses x as next step
-                    if key == self.routingNextStepTable[nextStep] and nextStep in distances:
-                        print(key, ' - deletado linha 210, distances')
-                        del distances[nextStep]
+                # if key in distances: # 1º don't send for x information to go to x
+                #     del distances[key]
+                # for nextStep in self.routingTable: # 2º don't send for x information that uses x as next step
+                #     if key == self.routingNextStepTable[nextStep] and nextStep in distances:
+                #         del distances[nextStep]
+                # if key in distances or key in distances.values():
+                #     del distances[key]
+                
+                for old in distances.copy():
+                    if key == old or key == distances[old]['next']:
+                        del distances[old]
 
                 message = { 
                     'type'  : 'update',
                     'source': self.myAddress ,  
                     'destination': key,
-                    'distances': distances
+                    'distances': { ip : distances[ip]['weight'] for ip in distances}
                 }
                 sock.sendto(str.encode( json.dumps( message ) ), (key, self.port))
 
@@ -248,15 +272,11 @@ class DCCRIP:
                 print(k , "---> " , self.neighborsTable[k])
 
             print('**** PESOS ----')
-            for k in self.routingWeightTable:
-                print(k , ' ---> ' , self.routingWeightTable[k]) 
-
-            print('**** NEXT -----')
-            for k in self.routingNextStepTable:
-                print(k , ' ---> ' , self.routingNextStepTable[k])
+            for k in self.routingTable:
+                print(k , ' ---> ' , self.routingTable[k]['weight'] , ' ---> ' , self.routingTable[k]['next']) 
             print("-"*40)
-        except:
-            print("ERROOO")
+        except Exception as e:
+            print(e)
     
         
 
